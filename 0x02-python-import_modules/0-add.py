8fe0621bdc704c52659468aa359892e8ad459c87
Kenneth Attans <kennykayock@gmail.com>
2023-01-05 18:47:41 -0800

    Task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,11 +7,7 @@
 if __name__ == "__main__":
 
     """Print the sum of 1 and 2."""
-
-            from add_0 import add
-
-                a = 1
-
-                    b = 2
-
-                        print("{} + {} = {}".format(a, b, add(a, b)))
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
