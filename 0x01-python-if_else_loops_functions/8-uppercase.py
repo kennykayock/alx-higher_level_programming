09c2148e809c5e38f3c3ef35d7a21fa25d576847
Kenneth Attans <kennykayock@gmail.com>
2023-01-06 04:37:11 -0800

    Task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-uppercase("holberton")
-uppercase("Holberton Scholl 98 Battery street")
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+    print("{:s}".format(i), end="")
+    print()
